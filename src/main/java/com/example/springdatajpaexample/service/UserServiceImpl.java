2e5b0039893e1e1e2cdac2b932772ffbca4cd0a9
kadun1 <kadunjj@gmail.com>
2023-02-27 22:34:10 +0900

    [Spring Data Jpa] review

--- src/main/java/com/example/springdatajpaexample/service/UserServiceImpl.java
+++ src/main/java/com/example/springdatajpaexample/service/UserServiceImpl.java
@@ -20,6 +20,7 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User findUser(String email) {
+
         Optional<User> userOpt = userRepository.findById("kadun@gmail.com");
 
         userOpt.ifPresent(userRepository::delete);
