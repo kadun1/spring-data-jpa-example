1f09b059afb122b0326d316332c024c72cc9a4fb
kadun1 <kadunjj@gmail.com>
2023-03-05 01:31:42 +0900

    [Spring Data Jpa] review

--- src/main/java/com/example/springdatajpaexample/exception/NoUserException.java
+++ src/main/java/com/example/springdatajpaexample/exception/NoUserException.java
@@ -8,5 +8,6 @@ public class NoUserException extends RuntimeException {
 
     public NoUserException(String message) {
         super(message);
+        System.out.println("유저가 존재하지 않습니다.");
     }
 }
